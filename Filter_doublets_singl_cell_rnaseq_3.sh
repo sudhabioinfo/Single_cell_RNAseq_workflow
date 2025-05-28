eaa54ded68a031ebe3918644ab583fa5f3e66a3e
sudhabioinfo <sgovindarajan6@gmail.com>
2025-05-28 14:18:23 +0530

    Update Filter_doublets_singl_cell_rnaseq_3.sh

--- Filter_doublets_singl_cell_rnaseq_3.sh
+++ Filter_doublets_singl_cell_rnaseq_3.sh
@@ -1,4 +1,6 @@
 # filter out doublets: DoubletFinder
+# DoubletFinder is an R package that: Simulates artificial doublets by averaging expression profiles of random real cells. Projects real and artificial cells into PCA space.
+# Uses k-nearest neighbors to determine how close each real cell is to the artificial doublets. Calculates a doublet score (pANN) for each cell. Classifies cells as “Singlet” or “Doublet” based on expected doublet rates (e.g., ~0.8% per 1,000 cells for 10X).
 # setwd("~/Desktop/demo/single_cell_doublets/scripts")
 
 #Dataset: https://www.10xgenomics.com/datasets/10k-human-pbmcs-3-v3-1-chromium-controller-3-1-high
